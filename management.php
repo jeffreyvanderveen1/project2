44c26544670848136f9b9665243c3f42f3f356b5
Jeffrey van der Veen <veen096@student.alfa-college.nl>
2017-11-08 11:09:57 +0100

    Beheerders pagina laat nu de gebruikers zien

--- management.php
+++ management.php
@@ -2,6 +2,12 @@
 
 <?php 
 session_start();
+
+
+global $con;
+
+$con = mysqli_connect("localhost", "root", "", "pro2");
+
 ?>
 
 
@@ -12,6 +18,7 @@ session_start();
 		<link rel="stylesheet" href="style.css">
 		<link rel="stylesheet" href="includes/navigation.css">
 		<link rel="stylesheet" href="includes/registration.css">
+		<link rel="stylesheet" href="includes/management.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	</head>
 
@@ -48,7 +55,43 @@ session_start();
 					else
 					{
 						?>
-						<p>lalalalalalal</p>
+						<h2>Beheerders</h2>
+						
+						<table id = "manage_users">
+						<th style = "width:300px;">Email</th><th style = "width:400px;">Voornaam + Achternaam</th><th>Beheerder</th>
+						<?php
+						$result = mysqli_query($con, "SELECT `email`, `voornaam`, `achternaam` FROM `users` WHERE `beheerder` = 1");
+						
+						while($row = mysqli_fetch_array($result))
+						{
+							?>
+							<tr>
+								<td><?php echo $row['email']; ?></td><td><?php echo $row['voornaam'] ." ". $row['achternaam']; ?></td>
+							</tr>
+							<?php
+						
+						}
+						?>
+						</table>
+
+						<h2>Gebruikers</h2>
+						
+						<table id = "manage_users">
+						<th style = "width:300px;">Email</th><th style = "width:400px;">Voornaam + Achternaam</th><th>Beheerder</th>
+						<?php
+						$result = mysqli_query($con, "SELECT `email`, `voornaam`, `achternaam` FROM `users` WHERE `beheerder` = 0");
+						while($row = mysqli_fetch_array($result))
+						{
+							?>
+							<tr>
+								<td><?php echo $row['email']; ?></td><td><?php echo $row['voornaam'] ." ". $row['achternaam']; ?></td>
+							</tr>
+							<?php
+						
+						}
+						?>
+						</table>
+						
 						<?php
 					}
 				}
